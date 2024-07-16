4fefb51d4523eadb0dab9221e7327d854ce293d8
Rishabh Joshi <rishabh.joshi01@thehutgroup.com>
2024-07-16 19:20:51 +0100

    Use specific credentials provider and endpoint

--- app/src/main/java/com/procure/thg/cockroachdb/App.java
+++ app/src/main/java/com/procure/thg/cockroachdb/App.java
@@ -1,29 +1,30 @@
 package com.procure.thg.cockroachdb;
 
 import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.SEVERE;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.logging.Logger;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.endpoints.S3EndpointProvider;
 
 public class App {
 
   private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
   private static final String THRESHOLD_SECONDS = "THRESHOLD_SECONDS";
-  private static final Region REGION = Region.US_EAST_1; // Change to your bucket's region
+  private static final Region REGION = Region.EU_WEST_1; // Change to your bucket's region
+  private static final String AWS_ENDPOINT_URL = "AWS_ENDPOINT_URL";
 
   public static void main(String[] args) {
     LOGGER.log(INFO, "Initialising S3 client...");
     final S3Client s3Client = S3Client.builder()
         .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-        .endpointProvider(S3EndpointProvider.defaultProvider())
+        .endpointOverride(getEndpointUri())
         .region(REGION)
+        .forcePathStyle(true)
         .build();
 
     final S3Cleaner cleaner = new S3Cleaner(s3Client, getThresholdSeconds());
@@ -33,33 +34,28 @@ public class App {
     LOGGER.log(INFO, "S3 client closed");
   }
 
-  public static long getThresholdSeconds() {
+  private static URI getEndpointUri() {
+    final var uri = System.getenv(AWS_ENDPOINT_URL);
+    if (uri == null || uri.isEmpty()) {
+      var msg = AWS_ENDPOINT_URL + " environment variable not set";
+      LOGGER.log(SEVERE, msg);
+      throw new IllegalArgumentException(msg);
+    }
+    try {
+      return new URI(uri);
+    } catch (URISyntaxException e) {
+      LOGGER.log(SEVERE, "Invalid endpoint URI: {0}", uri);
+      throw new IllegalArgumentException(e);
+    }
+  }
+
+  private static long getThresholdSeconds() {
     final var thresholdEnv = System.getenv(THRESHOLD_SECONDS);
-    if (thresholdEnv != null) {
-      return Long.parseLong(thresholdEnv);
+    if (thresholdEnv == null || thresholdEnv.isEmpty()) {
+      var msg = THRESHOLD_SECONDS + " environment variable not set";
+      LOGGER.log(SEVERE, msg);
+      throw new IllegalArgumentException(msg);
     }
-
-    var propertyName = "threshold_seconds";
-    var propertyValue = System.getProperty(propertyName);
-    if (propertyValue != null) {
-      return Long.parseLong(propertyValue);
-    }
-
-    final var properties = new Properties();
-    try (InputStream input = App.class.getClassLoader().getResourceAsStream("config.properties")) {
-      if (input == null) {
-        throw new IllegalArgumentException("config.properties file not found");
-      }
-      properties.load(input);
-    } catch (IOException e) {
-      throw new IllegalStateException("Failed to load config.properties", e);
-    }
-
-    final String thresholdProp = properties.getProperty(propertyName);
-    if (thresholdProp != null) {
-      return Long.parseLong(thresholdProp);
-    }
-
-    throw new IllegalArgumentException("Threshold hours not found in environment or properties file");
+    return Long.parseLong(thresholdEnv);
   }
 }
